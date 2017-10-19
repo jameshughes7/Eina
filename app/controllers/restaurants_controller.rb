291bda5dc33eb95fcd8288315ae808f83a902c4d
James Hughes <hugjimbo77@gmail.com>
2017-10-19 14:50:34 +0100

    show restaurant created: test failing

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,21 @@
 class RestaurantsController < ApplicationController
 
+  def create
+    @restaurant = Restaurant.new(params)
+
+    @restaurant.save
+    redirect_to @restaurant
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   def new
   end
 
-  def create
-    render plain: params[:restaurant].inspect
-  end
-
+  private
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :description, :location)
+    end
 end
--- spec/views/restaurants/index.html.erb_spec.rb
+++ spec/views/restaurants/index.html.erb_spec.rb
@@ -5,4 +5,14 @@ feature 'new restaurant' do
     visit 'restaurants/new'
     expect(page).to have_content ("New Restaurant")
   end
+
+  scenario 'show new restaurant created' do
+    visit 'restaurant/new'
+    fill_in "restaurant[name]", with: "Jimmy's Gatsby's"
+    fill_in "restaurant[description]", with: "Good old traditional gatsby"
+    fill_in "restaurant[location]", with: "Wynberg"
+    click_button "submit"
+    expect(page).to have_content(name: "Jimmy's Gatsby's", description: "Good old traditional gatsby", location: "Wynberg")
+  end
+
 end
